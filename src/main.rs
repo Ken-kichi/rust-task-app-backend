6d28abe63399a2fedede578f7a10f29738d40ca3
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-10-25 14:24:50 +0900

    JSON出力を実装

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Json, routing::get};
+use serde_json::{Value, json};
+
+async fn json() -> Json<Value> {
+    let data = json!({
+        "message": "Hello, JSON!"
+    });
+    Json(data)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(json));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> String {
-    "Hello, World!".to_string()
 }
